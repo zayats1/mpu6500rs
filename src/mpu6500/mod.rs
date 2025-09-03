065a6c5f6bd89079aedbb0c5730186bda4749f20
zayats1 <zaiats.bogdan@gmail.com>
2025-09-03 16:40:57 +0300

    disable the interrupt

--- src/mpu6500/mod.rs
+++ src/mpu6500/mod.rs
@@ -173,11 +173,11 @@ where
     where
         DELAY: delay::DelayNs,
     {
-        // Self::pwr_mgmt_1(self.addr, i2c, false, false, false, false, false, 0x01)?;
-        // Self::pwr_mgmt_2(self.addr, i2c, false, false, false, false, false, false)?;
+        Self::pwr_mgmt_1(self.addr, i2c, false, false, false, false, false, 0x01)?;
+        Self::pwr_mgmt_2(self.addr, i2c, false, false, false, false, false, false)?;
         delay.delay_ms(200);
 
-        Self::enable_interrupt(self.addr, i2c, false, false, false, false)?;
+        // Self::enable_interrupt(self.addr, i2c, false, false, false, false)?;
         Self::fifo_enable(
             self.addr, i2c, false, false, false, false, false, false, false, false,
         )?;
