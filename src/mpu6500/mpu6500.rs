a345f8b0ed4735484cf8184002779bd33f7f379f
zayats1 <zaiats.bogdan@gmail.com>
2025-09-06 13:12:02 +0300

    Refactor: split user and system functions

---
+++ src/mpu6500/mpu6500.rs
@@ -0,0 +1,324 @@
+use core::error::Error;
+use core::fmt;
+
+use core::marker::PhantomData;
+
+use embedded_hal::delay;
+use embedded_hal::i2c;
+use nalgebra::Vector3;
+
+
+use crate::mpu6500::config;
+use crate::mpu6500::config::Config;
+use crate::mpu6500::register;
+use crate::mpu6500::register::Register;
+
+use super::mpu6500_sys::accel_cfg;
+use super::mpu6500_sys::accel_cfg_2;
+use super::mpu6500_sys::enable_interrupt;
+use super::mpu6500_sys::fifo_count_h;
+use super::mpu6500_sys::fifo_enable;
+use super::mpu6500_sys::fifo_read;
+use super::mpu6500_sys::gyro_cfg;
+use super::mpu6500_sys::i2c_mst_ctrl;
+use super::mpu6500_sys::int_pin_bypass_enable_interrupt_cfg;
+use super::mpu6500_sys::pwr_mgmt_1;
+use super::mpu6500_sys::pwr_mgmt_2;
+use super::mpu6500_sys::read_accel_offset;
+use super::mpu6500_sys::read_register;
+use super::mpu6500_sys::sample_rate_div;
+use super::mpu6500_sys::user_ctrl;
+use super::mpu6500_sys::configure;
+use super::mpu6500_sys::who_am_i;
+use super::mpu6500_sys::write_accel_offset;
+use super::mpu6500_sys::write_gyro_offset;
+
+
+
+
+
+pub const I2C_ADDR_AL: u8 = 0x68;
+pub const I2C_ADDR_AH: u8 = 0x69;
+
+pub const DEV_ID_MPU6500: u8 = 0x70;
+pub const DEV_ID_MPU9250: u8 = 0x71;
+pub const DEV_ID_MPU9255: u8 = 0x73;
+
+// const CALIB_GYRO_SENSITIVITY: u16 = 131; // LSB/deg/s
+const CALIB_ACCEL_SENSITIVITY: u16 = 16384; // LSB/g
+
+const DEG_TO_RAD: f32 = 0.01745329252;
+
+#[derive(Debug,defmt::Format)]
+pub enum  Mpu6500Err<I2C> where
+    I2C: i2c::I2c,{
+     I2cErr(<I2C as embedded_hal::i2c::ErrorType>::Error),
+     InvalidDevice(u8),
+
+}
+
+impl <I2C> fmt::Display for  Mpu6500Err<I2C> where
+    I2C: i2c::I2c,{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        
+        match self {
+            Mpu6500Err::I2cErr(e) => write!(f, "I2C error: {:?}",e),
+            Mpu6500Err::InvalidDevice(dev_id) => write!(f, "The device is not a MPU6500: {}",dev_id) ,
+        }
+    }
+}
+
+impl  <I2C> Error for Mpu6500Err<I2C> where
+    I2C: i2c::I2c + core::fmt::Debug,{}
+
+pub struct Mpu6500<I2C>
+where
+    I2C: i2c::I2c,
+{
+    addr: u8,
+    cfg: Config,
+    pub(crate) acceleration: Vector3<f32>,
+    pub(crate) angular_velocity: Vector3<f32>,
+    pub(crate) temperature: f32,
+    accel_bias: Vector3<f32>,
+    gyro_bias: Vector3<f32>,
+    accel_resolution: f32,
+    gyro_resolution: f32,
+    _i2c: PhantomData<I2C>,
+}
+
+impl<I2C> Mpu6500<I2C>
+where
+    I2C: i2c::I2c,
+{
+    pub fn new<DELAY>(addr: u8, delay: &mut DELAY, i2c: &mut I2C) -> Result<Self, Mpu6500Err<I2C>>
+    where
+        DELAY: delay::DelayNs,
+    {
+        Self::with_configuration(addr, i2c, delay, Config::default())
+    }
+
+    pub fn with_configuration<DELAY>(
+        addr: u8,
+        i2c: &mut I2C,
+        delay: &mut DELAY,
+        cfg: Config,
+    ) -> Result<Self,Mpu6500Err<I2C>>
+    where
+        DELAY: delay::DelayNs,
+    {
+        let dev_id = who_am_i(addr, i2c).map_err(|e| Mpu6500Err::I2cErr(e))?;
+
+        if dev_id != DEV_ID_MPU6500 && dev_id != DEV_ID_MPU9250 && dev_id != DEV_ID_MPU9255 {
+            return Err(Mpu6500Err::InvalidDevice(dev_id));
+        }
+
+        let mut mpu = Self {
+            addr,
+            acceleration: Vector3::default(),
+            angular_velocity: Vector3::default(),
+            temperature: 0.0,
+            accel_bias: Vector3::default(),
+            gyro_bias: Vector3::default(),
+            accel_resolution: cfg.accel_fs_sel.get_resolution(),
+            gyro_resolution: cfg.gyro_fs_sel.get_resolution(),
+            cfg,
+            _i2c: PhantomData,
+        };
+
+        mpu.init(i2c, delay).map_err(|e| Mpu6500Err::I2cErr(e))?;
+
+        Ok(mpu)
+    }
+
+    fn init<DELAY>(&mut self, i2c: &mut I2C, delay: &mut DELAY) -> Result<(),<I2C as embedded_hal::i2c::ErrorType>::Error>
+    where
+        DELAY: delay::DelayNs,
+    {
+        pwr_mgmt_1(self.addr, i2c, false, false, false, false, false, 0x00)?;
+        delay.delay_ms(100);
+
+        pwr_mgmt_1(self.addr, i2c, false, false, false, false, false, 0x01)?;
+        delay.delay_ms(200);
+
+        configure(self.addr, i2c, false, self.cfg.gyro_dlpf_cfg)?;
+        sample_rate_div(self.addr, i2c, self.cfg.fifo_sample_rate)?;
+
+        gyro_cfg(self.addr, i2c, self.cfg.gyro_fs_sel, self.cfg.gyro_fchoice)?;
+
+        accel_cfg(self.addr, i2c, self.cfg.accel_fs_sel)?;
+        accel_cfg_2(
+            self.addr,
+            i2c,
+            (self.cfg.accel_fchoice & 0x01) == 1,
+            self.cfg.accel_dlpf_cfg,
+        )?;
+        int_pin_bypass_enable_interrupt_cfg(
+            self.addr, i2c, false, false, false, false, false, false, true,
+        )?;
+        enable_interrupt(self.addr, i2c, false, false, false, true)?;
+        delay.delay_ms(100);
+
+        Ok(())
+    }
+
+    pub(crate) fn read_imu(&mut self, i2c: &mut I2C) -> Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+        let mut buf = [0; 14];
+        read_register(self.addr, i2c, Register::ACCEL_XOUT_H, &mut buf)?;
+
+        self.acceleration = Vector3::new(
+            i16::from_be_bytes([buf[0], buf[1]]) as f32 * self.accel_resolution,
+            i16::from_be_bytes([buf[2], buf[3]]) as f32 * self.accel_resolution,
+            i16::from_be_bytes([buf[4], buf[5]]) as f32 * self.accel_resolution,
+        );
+
+        self.temperature = i16::from_be_bytes([buf[6], buf[7]]) as f32 / 333.87 + 21.0;
+
+        self.angular_velocity = Vector3::new(
+            i16::from_be_bytes([buf[8], buf[9]]) as f32 * self.gyro_resolution * DEG_TO_RAD,
+            i16::from_be_bytes([buf[10], buf[11]]) as f32 * self.gyro_resolution * DEG_TO_RAD,
+            i16::from_be_bytes([buf[12], buf[13]]) as f32 * self.gyro_resolution * DEG_TO_RAD,
+        );
+
+        Ok(())
+    }
+
+    pub fn read(&mut self, i2c: &mut I2C) -> Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+        self.read_imu(i2c)?;
+
+        Ok(())
+    }
+    /// the function either shuts down the gyro or makes it inacurate
+    pub fn calibrate<DELAY>(&mut self, i2c: &mut I2C, delay: &mut DELAY) -> Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error>
+    where
+        DELAY: delay::DelayNs,
+    {
+        pwr_mgmt_1(self.addr, i2c, false, false, false, false, false, 0x01)?;
+        pwr_mgmt_2(self.addr, i2c, false, false, false, false, false, false)?;
+        delay.delay_ms(200);
+
+        enable_interrupt(self.addr, i2c, false, false, false, false)?;
+        fifo_enable(
+            self.addr, i2c, false, false, false, false, false, false, false, false,
+        )?;
+        pwr_mgmt_1(self.addr, i2c, false, false, false, false, false, 0x00)?;
+        i2c_mst_ctrl(self.addr, i2c, false, false, false, false, 0x00)?;
+        user_ctrl(self.addr, i2c, false, false, false, false, false, false)?;
+        user_ctrl(self.addr, i2c, false, false, true, true, false, false)?;
+        delay.delay_ms(15);
+
+        configure(self.addr, i2c, false, config::GyroDlpfCfg::Dlpf184Hz)?;
+        sample_rate_div(self.addr, i2c, config::FifoSampleRate::Smpl1000Hz)?;
+        gyro_cfg(self.addr, i2c, config::GyroFullScaleSelect::Dps250, 0x00)?;
+        accel_cfg(self.addr, i2c, config::AccelFullScaleSelect::G2)?;
+
+        user_ctrl(self.addr, i2c, true, false, false, false, false, false)?;
+        fifo_enable(
+            self.addr, i2c, false, true, true, true, true, false, false, false,
+        )?;
+        delay.delay_ms(40);
+
+        fifo_enable(
+            self.addr, i2c, false, false, false, false, false, false, false, false,
+        )?;
+
+        let fifo_count = fifo_count_h(self.addr, i2c)?;
+        let packet_count = fifo_count / 12;
+
+        for _ in 0..packet_count {
+            let (accel, gyro) = fifo_read(self.addr, i2c)?;
+
+            self.accel_bias.x += accel.x as f32;
+            self.accel_bias.y += accel.y as f32;
+            self.accel_bias.z += accel.z as f32;
+
+            self.gyro_bias.x += gyro.x as f32;
+            self.gyro_bias.y += gyro.y as f32;
+            self.gyro_bias.z += gyro.z as f32;
+        }
+
+        if packet_count != 0 {
+            self.accel_bias /= packet_count as f32;
+            self.gyro_bias /= packet_count as f32;
+
+            if self.accel_bias.z > 0.0 {
+                self.accel_bias.z -= CALIB_ACCEL_SENSITIVITY as f32;
+            } else {
+                self.accel_bias.z += CALIB_ACCEL_SENSITIVITY as f32;
+            }
+        }
+
+        let mut accel_offset = read_accel_offset(self.addr, i2c)?;
+        let mut mask_bit = Vector3::new(1, 1, 1);
+
+        // x
+
+        if accel_offset.x % 2 != 0 {
+            mask_bit.x = 0;
+        }
+
+        accel_offset.x -= (self.accel_bias.x as i16) >> 3;
+
+        if mask_bit.x != 0 {
+            accel_offset.x &= !mask_bit.x;
+        } else {
+            accel_offset.x |= 0b1;
+        }
+
+        // y
+
+        if accel_offset.y % 2 != 0 {
+            mask_bit.y = 0;
+        }
+
+        accel_offset.y -= (self.accel_bias.y as i16) >> 3;
+
+        if mask_bit.y != 0 {
+            accel_offset.y &= !mask_bit.y;
+        } else {
+            accel_offset.y |= 0b1;
+        }
+
+        // z
+
+        if accel_offset.z % 2 != 0 {
+            mask_bit.z = 0;
+        }
+
+        accel_offset.z -= (self.accel_bias.z as i16) >> 3;
+
+        if mask_bit.z != 0 {
+            accel_offset.z &= !mask_bit.z;
+        } else {
+            accel_offset.z |= 0b1;
+        }
+
+        let gyro_offset = Vector3::new(
+            -self.gyro_bias.x as i16 / 4,
+            -self.gyro_bias.y as i16 / 4,
+            -self.gyro_bias.z as i16 / 4,
+        );
+
+        write_accel_offset(self.addr, i2c, accel_offset)?;
+        write_gyro_offset(self.addr, i2c, gyro_offset)?;
+
+        delay.delay_ms(100);
+
+        self.init(i2c, delay)?;
+
+        Ok(())
+    }
+
+    pub fn acceleration(&self) -> Vector3<f32> {
+        self.acceleration
+    }
+
+    pub fn angular_velocity(&self) -> Vector3<f32> {
+        self.angular_velocity
+    }
+
+
+    pub fn temperature(&self) -> f32 {
+        self.temperature
+    }
+}
---
+++ src/mpu6500/mpu6500_sys.rs
@@ -0,0 +1,374 @@
+
+
+
+use embedded_hal::i2c;
+use nalgebra::Vector3;
+
+use crate::mpu6500::config;
+use crate::mpu6500::register::Register;
+
+/// Registers 19 to 24 – Gyro Offset Registers
+pub (crate)fn write_gyro_offset<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+    gyro_offset: Vector3<i16>,
+) -> Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    let x = gyro_offset.x.to_be_bytes();
+    write_register(addr, i2c, Register::XG_OFFSET_H, x[0])?;
+    write_register(addr, i2c, Register::XG_OFFSET_L, x[1])?;
+
+    let y = gyro_offset.y.to_be_bytes();
+    write_register(addr, i2c, Register::YG_OFFSET_H, y[0])?;
+    write_register(addr, i2c, Register::YG_OFFSET_L, y[1])?;
+
+    let z = gyro_offset.z.to_be_bytes();
+    write_register(addr, i2c, Register::ZG_OFFSET_H, z[0])?;
+    write_register(addr, i2c, Register::ZG_OFFSET_L, z[1])?;
+
+    Ok(())
+}
+
+/// Register 25 – Sample Rate Divider
+pub (crate)fn sample_rate_div<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+    smplrt_div: config::FifoSampleRate,
+) -> Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    write_register(addr, i2c, Register::SMPLRT_DIV, smplrt_div as u8)
+}
+
+/// Register 26 – Configuration
+pub (crate)fn configure<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+    fifo_mode: bool,
+    dlpf_cfg: config::GyroDlpfCfg,
+) -> Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    write_register(
+        addr,
+        i2c,
+        Register::CFG,
+        (fifo_mode as u8) << 6 | (dlpf_cfg as u8) << 0,
+    )
+}
+
+/// Register 27 – Gyroscope Configuration
+pub (crate)fn gyro_cfg<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+    gyro_fs_sel: config::GyroFullScaleSelect,
+    fchoice_b: u8,
+) -> Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    let mut c = [0; 1];
+    read_register(addr, i2c, Register::GYRO_CFG, &mut c)?;
+
+    write_register(
+        addr,
+        i2c,
+        Register::GYRO_CFG,
+        c[0] & 0b11100111 | (gyro_fs_sel as u8) << 3 | (fchoice_b & 0b11) << 0,
+    )
+}
+
+/// Register 28 – Accelerometer Configuration
+pub (crate)fn accel_cfg<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+    accel_fs_sel: config::AccelFullScaleSelect,
+) -> Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    let mut c = [0; 1];
+    read_register(addr, i2c, Register::ACCEL_CFG_2, &mut c)?;
+
+    write_register(
+        addr,
+        i2c,
+        Register::ACCEL_CFG,
+        c[0] & 0b11100111 | (accel_fs_sel as u8) << 3,
+    )
+}
+
+/// Register 29 – Accelerometer Configuration 2
+pub (crate)fn accel_cfg_2<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+    accel_fchoice_b: bool,
+    a_dlpf_cfg: config::AccelDlpfCfg,
+) -> Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    let mut c = [0; 1];
+    read_register(addr, i2c, Register::ACCEL_CFG_2, &mut c)?;
+
+    write_register(
+        addr,
+        i2c,
+        Register::ACCEL_CFG_2,
+        c[0] & 0b11110000 | (accel_fchoice_b as u8) << 3 | (a_dlpf_cfg as u8) << 0,
+    )
+}
+
+/// Register 35 – FIFO Enable
+pub (crate)fn fifo_enable<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+    temp_out: bool,
+    gyro_xout: bool,
+    gyro_yout: bool,
+    gyro_zout: bool,
+    accel: bool,
+    slv_2: bool,
+    slv_1: bool,
+    slv_0: bool,
+) -> Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    write_register(
+        addr,
+        i2c,
+        Register::FIFO_EN,
+        (temp_out as u8) << 7
+            | (gyro_xout as u8) << 6
+            | (gyro_yout as u8) << 5
+            | (gyro_zout as u8) << 4
+            | (accel as u8) << 3
+            | (slv_2 as u8) << 2
+            | (slv_1 as u8) << 1
+            | (slv_0 as u8) << 0,
+    )
+}
+
+/// Register 36 – I2C Master Control
+pub (crate)fn i2c_mst_ctrl<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+    mult_mst_en: bool,
+    wait_for_es: bool,
+    slv_3_fifo_en: bool,
+    i2c_mst_p_nsr: bool,
+    i2c_mst_clk: u8,
+) -> Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    write_register(
+        addr,
+        i2c,
+        Register::I2C_MST_CTRL,
+        (mult_mst_en as u8) << 7
+            | (wait_for_es as u8) << 6
+            | (slv_3_fifo_en as u8) << 5
+            | (i2c_mst_p_nsr as u8) << 4
+            | (i2c_mst_clk & 0b1111) << 0,
+    )
+}
+
+/// Register 55 – INT Pin / Bypass Enable Configuration
+pub (crate)fn int_pin_bypass_enable_interrupt_cfg<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+    actl: bool,
+    open: bool,
+    latch_int_en: bool,
+    int_anyrd_2clear: bool,
+    actl_fsync: bool,
+    fsync_int_mode_en: bool,
+    bypass_en: bool,
+) -> Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    write_register(
+        addr,
+        i2c,
+        Register::INT_PIN_CFG,
+        (actl as u8) << 7
+            | (open as u8) << 6
+            | (latch_int_en as u8) << 5
+            | (int_anyrd_2clear as u8) << 4
+            | (actl_fsync as u8) << 3
+            | (fsync_int_mode_en as u8) << 2
+            | (bypass_en as u8) << 1,
+    )
+}
+
+/// Register 56 – Interrupt Enable
+pub (crate)fn enable_interrupt<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+    wom_en: bool,
+    fifo_overflow_en: bool,
+    fsync_int_en: bool,
+    raw_rdy_en: bool,
+) -> Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    write_register(
+        addr,
+        i2c,
+        Register::INT_ENABLE,
+        (wom_en as u8) << 6
+            | (fifo_overflow_en as u8) << 4
+            | (fsync_int_en as u8) << 3
+            | (raw_rdy_en as u8) << 0,
+    )
+}
+
+/// Register 106 – User Control
+pub (crate)fn user_ctrl<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+    fifo_en: bool,
+    i2c_mst_en: bool,
+    i2c_if_dis: bool,
+    fifo_rst: bool,
+    i2c_mst_rst: bool,
+    sig_cond_rst: bool,
+) -> Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    write_register(
+        addr,
+        i2c,
+        Register::USER_CTRL,
+        (fifo_en as u8) << 6
+            | (i2c_mst_en as u8) << 5
+            | (i2c_if_dis as u8) << 4
+            | (fifo_rst as u8) << 2
+            | (i2c_mst_rst as u8) << 1
+            | (sig_cond_rst as u8) << 0,
+    )
+}
+
+/// Register 107 – Power Management 1
+pub (crate)fn pwr_mgmt_1<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+    h_reset: bool,
+    sleep: bool,
+    cycle: bool,
+    gyro_standby: bool,
+    pd_ptat: bool,
+    clksel: u8,
+) -> Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    write_register(
+        addr,
+        i2c,
+        Register::PWR_MGMT_1,
+        (h_reset as u8) << 7
+            | (sleep as u8) << 6
+            | (cycle as u8) << 5
+            | (gyro_standby as u8) << 4
+            | (pd_ptat as u8) << 3
+            | (clksel & 0b111) << 0,
+    )
+}
+
+// Register 108 – Power Management 2
+pub (crate)fn pwr_mgmt_2<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+    disable_xa: bool,
+    disable_ya: bool,
+    disable_za: bool,
+    disable_xg: bool,
+    disable_yg: bool,
+    disable_zg: bool,
+) -> Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    write_register(
+        addr,
+        i2c,
+        Register::PWR_MGMT_2,
+        (disable_xa as u8) << 5
+            | (disable_ya as u8) << 4
+            | (disable_za as u8) << 3
+            | (disable_xg as u8) << 2
+            | (disable_yg as u8) << 1
+            | (disable_zg as u8) << 0,
+    )
+}
+
+/// Register 114 – FIFO Count High
+pub (crate)fn fifo_count_h<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+) -> Result<u16, <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    let mut buf = [0; 2];
+    read_register(addr, i2c, Register::FIFO_COUNTH, &mut buf)?;
+    Ok(u16::from_be_bytes(buf))
+}
+
+/// Register 116 – FIFO Read Write
+pub (crate)fn fifo_read<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+) -> Result<(Vector3<i16>, Vector3<i16>), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    let mut buf = [0; 12];
+    read_register(addr, i2c, Register::FIFO_R_W, &mut buf)?;
+    Ok((
+        Vector3::new(
+            i16::from_be_bytes([buf[0], buf[1]]),
+            i16::from_be_bytes([buf[2], buf[3]]),
+            i16::from_be_bytes([buf[4], buf[5]]),
+        ),
+        Vector3::new(
+            i16::from_be_bytes([buf[6], buf[7]]),
+            i16::from_be_bytes([buf[8], buf[9]]),
+            i16::from_be_bytes([buf[10], buf[11]]),
+        ),
+    ))
+}
+
+/// Register 117 – Who Am I
+pub (crate)fn who_am_i<I2C: i2c::I2c>(addr: u8, i2c: &mut I2C) -> Result<u8, <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    let mut buf = [0; 1];
+    read_register(addr, i2c, Register::WHO_AM_I, &mut buf)?;
+    Ok(buf[0])
+}
+
+/// Registers 119, 120, 122, 123, 125, 126 – Accelerometer Offset Registers
+pub (crate)fn read_accel_offset<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+) -> Result<Vector3<i16>, <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    let mut buf = [0; 2];
+    read_register(addr, i2c, Register::XA_OFFSET_H, &mut buf)?;
+    let x = i16::from_be_bytes(buf);
+
+    let mut buf = [0; 2];
+    read_register(addr, i2c, Register::YA_OFFSET_H, &mut buf)?;
+    let y = i16::from_be_bytes(buf);
+
+    let mut buf = [0; 2];
+    read_register(addr, i2c, Register::ZA_OFFSET_H, &mut buf)?;
+    let z = i16::from_be_bytes(buf);
+
+    Ok(Vector3::new(x, y, z))
+}
+
+/// Registers 119, 120, 122, 123, 125, 126 – Accelerometer Offset Registers
+pub (crate)fn write_accel_offset<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+    accel_offset: Vector3<i16>,
+) -> core::result::Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    let x = accel_offset.x.to_be_bytes();
+    write_register(addr, i2c, Register::XA_OFFSET_H, x[0])?;
+    write_register(addr, i2c, Register::XA_OFFSET_L, x[1])?;
+
+    let y = accel_offset.y.to_be_bytes();
+    write_register(addr, i2c, Register::YA_OFFSET_H, y[0])?;
+    write_register(addr, i2c, Register::YA_OFFSET_L, y[1])?;
+
+    let z = accel_offset.z.to_be_bytes();
+    write_register(addr, i2c, Register::ZA_OFFSET_H, z[0])?;
+    write_register(addr, i2c, Register::ZA_OFFSET_L, z[1])?;
+
+    Ok(())
+}
+
+pub (crate)fn read_register<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+    reg: Register,
+    buf: &mut [u8],
+) -> core::result::Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    // i2c.write(addr, &[reg as u8])?;
+    // i2c.read(addr, buf)?;
+    i2c.write_read(addr, &[reg as u8], buf)?;
+    Ok(())
+}
+
+pub (crate)fn write_register<I2C: i2c::I2c>(
+    addr: u8,
+    i2c: &mut I2C,
+    reg: Register,
+    cmd: u8,
+) -> core::result::Result<(), <I2C as embedded_hal::i2c::ErrorType>::Error> {
+    i2c.write(addr, &[reg as u8, cmd])
+}
