ce66f59b0bfce92bf6258e9c65e7f4e7c4308ca7
zayats1 <zaiats.bogdan@gmail.com>
2025-11-23 22:15:37 +0200

    format code

--- src/units.rs
+++ src/units.rs
@@ -1,7 +1,6 @@
 use core::f32;
 
-const G_UNIT:f32 = 9.80665;
-
+const G_UNIT: f32 = 9.80665;
 
 /// parses some value from be bytes : like acceleration and angular velocity
 pub const fn val_from_bytes(bytes: [u8; 2], resolution: f32) -> f32 {
@@ -16,13 +15,10 @@ pub const fn deg_to_rad(deg: f32) -> f32 {
     deg * f32::consts::PI / 180.0
 }
 
-
 // converts g unit to m/s^2
-pub const fn  g_to_m_s2(acc_g: f32) -> f32 {
-   acc_g*G_UNIT
+pub const fn g_to_m_s2(acc_g: f32) -> f32 {
+    acc_g * G_UNIT
 }
-
-
 
 #[cfg(test)]
 mod unit_tests {
@@ -38,11 +34,11 @@ mod unit_tests {
     }
 
     #[test]
-    fn g_to_m_s2_test(){
-         let g = 1.0;
-         let expected = G_UNIT;
-         let diff = (g_to_m_s2(g) - expected).abs();
-         assert!(diff <= TOLLERANCE)
+    fn g_to_m_s2_test() {
+        let g = 1.0;
+        let expected = G_UNIT;
+        let diff = (g_to_m_s2(g) - expected).abs();
+        assert!(diff <= TOLLERANCE)
     }
 
     #[test]
@@ -62,14 +58,14 @@ mod unit_tests {
         assert_almost_eq(temperature_from_bytes(bytes), 25.00, NO_SO_PRECISE);
         let bytes = (-8680i16).to_be_bytes(); //  -5 degree C
         assert_almost_eq(temperature_from_bytes(bytes), -5.00, NO_SO_PRECISE);
-         let bytes = (-7011i16).to_be_bytes(); //  0 degree C
+        let bytes = (-7011i16).to_be_bytes(); //  0 degree C
         assert_almost_eq(temperature_from_bytes(bytes), 0.00, NO_SO_PRECISE);
     }
 
-        #[test]
+    #[test]
     fn val_from_bytes_test() {
         let resolution = GyroFullScaleSelect::Dps500.get_resolution(); // 500.0 / 32768.0,
         let bytes = 65i16.to_be_bytes();
-        assert_almost_eq(val_from_bytes(bytes,resolution), 1.0, NO_SO_PRECISE);
+        assert_almost_eq(val_from_bytes(bytes, resolution), 1.0, NO_SO_PRECISE);
     }
 }
